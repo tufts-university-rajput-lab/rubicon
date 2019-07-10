b07ebd0d32a3968fd6c120579f87590ec8bdd3ae
Matt Bliss <mmbliss81@gmail.com>
2019-07-10 13:06:49 -0400

    parmchk2, tleap updates
    Allows parmchk or parmchk2.
    Added _tleap_runner method.

--- rubicon/io/amber/antechamber.py
+++ rubicon/io/amber/antechamber.py
@@ -16,6 +16,7 @@ from monty.dev import requires
 from monty.os.path import which
 from monty.tempfile import ScratchDir
 
+from pymatgen import Molecule
 from pymatgen.io.lammps.data import Topology
 from rubicon.io.lammps.topology import correct_corrupted_top_files, \
     TopCorruptionException
@@ -32,29 +33,34 @@ class AntechamberRunner(object):
     A wrapper for AntechamberRunner software
     """
 
-    @requires(which('parmchk2'), "Requires the binary parmchk."
+    @requires((which('parmchk') or which('parmchk2')), "Requires the binary parmchk."
                                 "Install AmberTools from http://ambermd.org/#AmberTools")
     @requires(which('antechamber'), "Requires the binary antechamber."
                                     "Install AmberTools from http://ambermd.org/#AmberTools")
+
     def __init__(self, mols):
         """
         Args:
             mols: List of molecules
         """
         self.mols = mols
+        if which('parmchk'):
+            self.parmchk_version = 'parmchk'
+        else:
+            self.parmchk_version = 'parmchk2'
 
-    def _run_parmchk(self, filename="ANTECHAMBER_AC.AC", format="ac", outfile_name="mol.frcmod",
+    def _run_parmchk(self, filename="mol.mol2", format="mol2", outfile_name="mol.frcmod",
                      print_improper_dihedrals="Y"):
         """
         run parmchk
         """
-        command = "parmchk -i {} -f {} -o {} -w {}".format(filename, format, outfile_name,
+        command = parmchk_version + " -i {} -f {} -o {} -w {}".format(filename, format, outfile_name,
                                                            print_improper_dihedrals)
         exit_code = subprocess.call(shlex.split(command))
         return exit_code
 
     def _run_antechamber(self, filename, infile_format="gout", outfile_name="mol",
-                         outfile_format="ac", charge_method="resp", status_info=2):
+                         outfile_format="mol2", charge_method="resp", status_info=2):
         """
         run antechamber using the provided gaussian output file
         """
@@ -69,6 +75,31 @@ class AntechamberRunner(object):
         # GeneralizedForceFiled tries to read in *.ac(antechamber format) file and Toplogy
         # is trying to readin *.rtf(charmm format topology) file !!! WHY?!!
         # command = 'antechamber -i ' + filename + " -fi gout -o mol -fo charmm -c resp -s 2"
+        exit_code = subprocess.call(shlex.split(command))
+        return exit_code
+
+    def _run_tleap(self, mol_name='mol', amb_ff_version='ff14SB'):
+        '''
+        run tleap
+        '''
+        lines = []
+        lines.append('source leaprc.{}'.format(amb_ff_version))
+        lines.append('source leaprc.gaff')
+        lines.append('{} = loadmol2 {}.mol2'.format(mol_name,mol_name))
+        lines.append('check {}'.format(mol_name))
+        lines.append('loadamberparams {}.frcmod'.format(mol_name))
+        lines.append('saveoff {} {}.lib'.format(mol_name,mol_name))
+        lines.append('saveamberparm {} {}.prmtop {}.inpcrd'.format(mol_name,mol_name,mol_name))
+        lines.append('quit')
+
+        text = '\n'.join(lines)
+
+        file = open('tleap.in', 'w')
+        file.write(text)
+        file.close()
+
+        command = 'tleap -f tleap.in'
+
         exit_code = subprocess.call(shlex.split(command))
         return exit_code
 
@@ -95,10 +126,12 @@ class AntechamberRunner(object):
             self._run_parmchk()
             # if antechamber can't find parameters go to gaff_example.dat
             try:
-                top = Topology.from_file('mol.rtf')
+                mol = Molecule.from_file('mol.rtf')
+                print('mol.rtf file exists')
             except TopCorruptionException:
                 correct_corrupted_top_files('mol.rtf', 'gaff_data.txt')
                 top = Topology.from_file('mol.rtf')
+                print('mol.rtf file does not exist')
             try:
                 gff = ForceField.from_file('mol.frcmod')
             except FFCorruptionException:
